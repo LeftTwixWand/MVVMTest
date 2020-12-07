68dc9de5ce040bf1e8dd7d94cdd0b8d1c3c743ae
Vladyslav Horbachov <0681813464.dev@gmail.com>
2020-12-07 17:28:07 +0200

    Add project files.

---
+++ MVVMTest/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MVVMTest.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+            LoadCommand = new AsyncRelayCommand<string>(Load);
+        }
+
+        /// <summary>
+        /// Gets the <see cref="IAsyncRelayCommand{T}"/> responsible for loading the source markdown docs.
+        /// </summary>
+        public IAsyncRelayCommand<string> LoadCommand { get; }
+
+        private async Task Load(string arg)
+        {
+            await Task.Delay(500);
+            Debug.WriteLine("Loaded event!");
+        }
+    }
+}
